46ddaa8b3e56536980d545c3335ceb3639ed0e4c
Yiching Tseng <yictseng@z4r5p4.le-101.fr>
2019-10-10 16:39:41 +0200

    update 10 oct

---
+++ libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   libft.h                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: yictseng <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/08 12:39:07 by yictseng     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/09 16:33:54 by yictseng    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+size_t	ft_strlen(const char *s);
+#endif
