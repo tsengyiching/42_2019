9ce8b53b0d2ab6e4e5d09e2a01dfddb3de286ee6
Yiching Tseng <yictseng@z4r8p1.le-101.fr>
2019-10-31 12:27:45 +0100

    check *.c and makefile

---
+++ libft/ft_split.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_split.c                                       .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: yictseng <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/23 19:13:41 by yictseng     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/28 15:44:24 by yictseng    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	is_char(char const s, char c)
+{
+	if (s == c)
+		return (1);
+	return (0);
+}
+
+static int	nb_arr(char const *s, char c)
+{
+	int count;
+
+	count = 0;
+	if (*s && !is_char(*s, c))
+		count++;
+	s++;
+	while (*s)
+	{
+		if (!is_char(*s, c) && is_char(*(s - 1), c))
+			count++;
+		while (*s && !is_char(*s, c))
+			s++;
+		while (*s && is_char(*s, c))
+			s++;
+	}
+	return (count);
+}
+
+static int	put_arr(char const *s, char c, char **tab, int *i)
+{
+	int j;
+	int k;
+
+	while (*(s + *i) && is_char(*(s + *i), c))
+		(*i)++;
+	j = 0;
+	while (*(s + *i + j) && !is_char(*(s + *i + j), c))
+		j++;
+	if (!(*tab = malloc(sizeof(char) * (j + 1))))
+		return (0);
+	k = 0;
+	while (*(s + *i) && !is_char(*(s + *i), c))
+	{
+		(*tab)[k] = *(s + *i);
+		k++;
+		(*i)++;
+	}
+	(*tab)[k] = '\0';
+	return (1);
+}
+
+char		**ft_split(char const *s, char c)
+{
+	char	**tab;
+	int		nb;
+	int		i;
+	int		arr;
+
+	i = 0;
+	if (!s)
+		return (0);
+	nb = nb_arr(s, c);
+	if (!(tab = malloc(sizeof(char *) * (nb + 1))))
+		return (NULL);
+	tab[nb] = 0;
+	arr = 0;
+	while (arr < nb)
+	{
+		if (!(put_arr(s, c, &tab[arr], &i)))
+			return (ft_free_tab(tab));
+		arr++;
+	}
+	return (tab);
+}
